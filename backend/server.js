9785a0ce5a442fc5f6102cf4619bed8b5134d136
mcontoor <mcontoor@gmail.com>
2023-04-17 22:20:04 +0530

    enable cors in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000'
+};
+  
+app.use(cors(corsOptions));
 
 app.listen(4000, () => {
   console.log('Server is listening on port 4000');
